555467d76c981a5f853a99225ae8a8132d63585a
vectorxz <vectorx2263@hotmail.com>
2020-03-01 11:46:10 +0700

    import firebase and applying email login auth

--- app/src/main/java/com/example/wirelessmatchinggame/MainActivity.kt
+++ app/src/main/java/com/example/wirelessmatchinggame/MainActivity.kt
@@ -6,13 +6,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
 
         val regisBtn: Button = findViewById<Button>(R.id.regisBtn)
         val loginBtn: Button = findViewById<Button>(R.id.loginBtn)
@@ -22,9 +28,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         loginBtn.setOnClickListener{
-            val username = usernameField.text.toString()
+            val email = emailField.text.toString()
             val password = passwordField.text.toString()
-            login(username, password)
+            login(email, password)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null) {
+            //TODO Switch to game page
         }
     }
 
@@ -33,19 +48,35 @@ class MainActivity : AppCompatActivity() {
         startActivity(regisIntent)
     }
 
-    fun login(username: String, password: String) {
+    fun login(email: String, password: String) {
 
-        if(username.length <= 0 || password.length <= 0) {
+        if(email.length <= 0 || password.length <= 0) {
             var errorMsg = "Please input";
-            if (username.length <= 0) {
-                errorMsg = errorMsg + " Username"
+            if (email.length <= 0) {
+                errorMsg = errorMsg + " email"
             }
             if (password.length <= 0) {
                 errorMsg = errorMsg + " Password"
             }
             Toast.makeText(this, errorMsg, Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+            // [START sign_in_with_email]
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //TODO Login success -> Redirect to game page
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed!", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // [START_EXCLUDE]
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "System error, please contact admin.", Toast.LENGTH_SHORT).show()
+                    }
+                    // [END_EXCLUDE]
+                }
+            // [END sign_in_with_email]
         }
     }
 }

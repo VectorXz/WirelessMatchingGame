a0d58bdba1a9e9aa1dd846c1ca9cc4105a08328a
ahfyks <dujnapa.tan@student.mahidol.edu>
2020-04-04 00:42:21 +0700

    FEAT[WIP]: added select image landing v1

---
+++ app/src/main/java/com/example/wirelessmatchinggame/SelectImageLanding.kt
@@ -0,0 +1,12 @@
+package com.example.wirelessmatchinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectImageLanding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_image_landing)
+    }
+}

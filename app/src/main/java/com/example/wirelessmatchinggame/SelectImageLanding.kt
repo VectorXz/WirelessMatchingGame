8a80af8708dee2258e70eba481e1cf665b794324
vectorxz <vectorx2263@hotmail.com>
2020-04-04 14:30:06 +0700

    update select image landing layout

--- app/src/main/java/com/example/wirelessmatchinggame/SelectImageLanding.kt
+++ app/src/main/java/com/example/wirelessmatchinggame/SelectImageLanding.kt
@@ -1,12 +1,42 @@
 package com.example.wirelessmatchinggame
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SelectImageLanding : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_image_landing)
+
+        val takePhoto: ImageButton = findViewById(R.id.camBtn1)
+        takePhoto.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val imgView : ImageView = findViewById(R.id.imgThumb1)
+            imgView.setImageBitmap(imageBitmap)
+        }
     }
 }

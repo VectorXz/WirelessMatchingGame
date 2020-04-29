4037d748245b9fcc3a3320389f94f8c2469ded35
vectorxz <vectorx2263@hotmail.com>
2020-04-29 23:51:05 +0700

    add read and show statistics from firebase

---
+++ app/src/main/java/com/example/wirelessmatchinggame/Statistic.kt
@@ -0,0 +1,11 @@
+package com.example.wirelessmatchinggame
+
+import com.google.firebase.Timestamp
+
+class Statistic {
+    val user: String = ""
+    val count: String = ""
+    val time: String = ""
+    val result: String = ""
+    val date: Timestamp = Timestamp.now()
+}
--- app/src/main/java/com/example/wirelessmatchinggame/ViewStatistics.kt
+++ app/src/main/java/com/example/wirelessmatchinggame/ViewStatistics.kt
@@ -1,14 +1,80 @@
 package com.example.wirelessmatchinggame
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class ViewStatistics : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+
+    var db = FirebaseFirestore.getInstance()
+
+    private lateinit var statsReference: DatabaseReference
+
+    private inner class ProductViewHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
+        internal fun setUser(user: String) {
+            val textView = view.findViewById<TextView>(R.id.userTxt)
+            textView.text = user
+        }
+
+        internal fun setCount(count: String) {
+            val textView = view.findViewById<TextView>(R.id.countTxt)
+            textView.text = count+" flips"
+        }
+
+        internal fun setResult(result: String) {
+            val textView = view.findViewById<TextView>(R.id.resultTxt)
+            textView.text = result
+        }
+
+        internal fun setTime(time: String) {
+            val textView = view.findViewById<TextView>(R.id.timeTxt)
+            textView.text = time+" seconds"
+        }
+
+        internal fun setDateTime(dateTime: Timestamp) {
+            val textView = view.findViewById<TextView>(R.id.datetimeTxt)
+            val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+            val netDate = Date(dateTime.seconds.toLong()*1000)
+            textView.text = sdf.format(netDate)
+        }
+    }
+
+    private inner class ProductFirestoreRecyclerAdapter internal constructor(options: FirestoreRecyclerOptions<Statistic>) : FirestoreRecyclerAdapter<Statistic, ProductViewHolder>(options) {
+        override fun onBindViewHolder(productViewHolder: ProductViewHolder, position: Int, productModel: Statistic) {
+            productViewHolder.setUser(productModel.user)
+            productViewHolder.setDateTime(productModel.date)
+            productViewHolder.setCount(productModel.count)
+            productViewHolder.setResult(productModel.result)
+            productViewHolder.setTime(productModel.time)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_stats, parent, false)
+            return ProductViewHolder(view)
+        }
+    }
+
+    private var adapter: ProductFirestoreRecyclerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +116,41 @@ class ViewStatistics : AppCompatActivity() {
                 }
             }
             false
+        }
+
+        val query = db!!.collection("statistics-db").document("stats").collection("vectorx2263@hotmail.com").orderBy("date", Query.Direction.DESCENDING)
+        val options = FirestoreRecyclerOptions.Builder<Statistic>().setQuery(query, Statistic::class.java).build()
+        val recView = findViewById<RecyclerView>(R.id.recyclerView)
+        recView.layoutManager = LinearLayoutManager(this)
+
+        adapter = ProductFirestoreRecyclerAdapter(options)
+        recView.adapter = adapter
+
+
+
+
+        db.collection("statistics-db").document("stats").collection("vectorx2263@hotmail.com")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("ViewStatistics", "document data >> "+ document.data)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("ViewStatistics", "Error getting documents.", exception)
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (adapter != null) {
+            adapter!!.stopListening()
         }
     }
 }

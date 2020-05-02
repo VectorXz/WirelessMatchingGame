73d8291ac2b73a5a9fdca2174094e56a2888b037
ahfyks <dujnapa.tan@student.mahidol.edu>
2020-05-02 15:28:10 +0700

    FEAT[WIP]: added graph in view_stats

--- app/src/main/java/com/example/wirelessmatchinggame/ViewStatistics.kt
+++ app/src/main/java/com/example/wirelessmatchinggame/ViewStatistics.kt
@@ -18,6 +18,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.jjoe64.graphview.helper.GraphViewXML
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.DataPointInterface
+import com.jjoe64.graphview.series.LineGraphSeries
+import com.jjoe64.graphview.series.Series
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -133,6 +138,12 @@ class ViewStatistics : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.w("ViewStatistics", "Error getting documents.", exception)
             }
+
+        val graph = findViewById<GraphViewXML>(R.id.graphViewXML)
+        val data1 = arrayOf ( DataPoint(0.12, 3.22) , DataPoint(0.22,0.69))
+        val seriesD = LineGraphSeries<DataPoint>(data1);
+        graph.removeAllSeries()
+        graph.addSeries(seriesD)
     }
 
     override fun onStart() {
